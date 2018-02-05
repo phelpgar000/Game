b97765eb4b597d01319ccc2543858dabaa7d4645
phelpgar000 <phelpgar000@warren.k12.in.us>
2018-02-05 11:16:53 -0500

    new changes

--- src/Board.java
+++ src/Board.java
@@ -1,2 +1,13 @@
-public class Board {
+import javax.swing.*;
+import java.awt.*;
+
+public class Board extends JFrame{
+
+    public Board(){
+
+        setPreferredSize(new Dimension(800, 600));
+        setBackground(Color.MAGENTA);
+
+    }
+
 }
--- src/Game.java
+++ src/Game.java
@@ -4,6 +4,13 @@ public class Game extends JFrame{
 
     public Game(){
 
+        setResizable(false);
+        setVisible(true);
+        setFocusable(true);
+        setTitle("Suck it Hugh Hefneir");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+
     }
 
 }
